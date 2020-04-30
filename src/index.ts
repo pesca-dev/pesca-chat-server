1f48f1812e46e274e7c30fa93eb59805721805ea
Louis Meyer <h1ghbre4k3r@dev.bre4k3r.de>
2020-04-30 17:57:56 +0200

    Add first basic SocketServer without any functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, World!");
+import { Server, Client } from "socket-chat-protocol";
+import { Socket } from "socket.io";
+
+export class SocketServer implements Server.SocketEventManager {
+    public register<K extends keyof Server.Event>(socket: Socket, event: K, fn: (data: Server.Event[K]) => void): void {
+        socket.on(event, fn);
+    }
+
+    public emit<K extends keyof Client.Event>(socket: Socket, event: K, data: Client.Event[K]): void {
+        socket.emit(event, data);
+    }
+}
