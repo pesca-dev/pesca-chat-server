703d1893d9e2ade777f3973baf14e6fc08f06d8f
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-01-04 23:29:04 +0100

    Set default timestamp for logging

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { makeServer } from "./server";
 import { makeSocketModule } from "./socket";
 import { makeAuthModule } from "./auth";
 import { makeCreateTextChannel } from "./channel/textChannel";
+import $ from "logsen";
+
+$.setTimestamp($.defaultTimestamp);
 
 const { authenticate } = makeAuthModule();
 const createTextChannel = makeCreateTextChannel({ makeId });
