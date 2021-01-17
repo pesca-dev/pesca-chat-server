0b84da26f6a72247b97bdbd243191887dd2411ee
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2021-01-17 01:19:24 +0100

    Add first echo server

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module git.pesca.dev/pesca-dev/pesca-chat-server
 
 go 1.15
 
-require github.com/gorilla/websocket v1.4.2 // indirect
+require github.com/gorilla/websocket v1.4.2
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,8 +1,45 @@
 package server
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var addr = "localhost:8080"
+
+var upgrader = websocket.Upgrader{}
+
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	c, err := upgrader.Upgrade(w, r, nil)
+	defer c.Close()
+
+	if err != nil {
+		log.Print("An error during upgrading: ", err)
+		return
+	}
+
+	for {
+		messageType, message, err := c.ReadMessage()
+
+		if err != nil {
+			log.Println("Error during reading from websocket: ", err)
+			break
+		}
+
+		log.Printf("Received: %s", message)
+		err = c.WriteMessage(messageType, message)
+
+		if err != nil {
+			log.Println("Error during writing: ", err)
+			break
+		}
+	}
+}
 
 // Server does magic
 func Server() {
-	fmt.Print("Hello, World!\n")
+	http.HandleFunc("/", handleRoot)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
